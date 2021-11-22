2fc5bdb05041f50c5fd78df038aae5f4da12bf1c
George Magdy <georgtriple@gmail.com>
2021-11-22 22:45:21 +0200

    Change object to a class

--- js/main.js
+++ js/main.js
@@ -1,41 +1,53 @@
-let books = [];
-
-const bookDiv = document.querySelector('#books');
-let removeBtns = document.querySelectorAll('.remove');
-function displayBooks() {
-  bookDiv.innerHTML = '';
-  if (books) {
-    books.forEach((book, index) => {
-      const bookElement = `<p>${book.title}</p>
-  <p>${book.author}</p>
-  <button class="remove" id="${index}">Remove</button>
-  <hr>`;
-      bookDiv.innerHTML += bookElement;
-    });
-    removeBtns = document.querySelectorAll('.remove');
+let removeBtns = document.querySelectorAll(".remove");
+class Book {
+  static books = [];
+  static #bookDiv = document.querySelector("#books");
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
+  add() {
+    Book.books.push(this);
+  }
+  static remove(bookIndex) {
+    Book.books.splice(bookIndex, 1);
+  }
+  static displayBooks() {
+    this.#bookDiv.innerHTML = "";
+    if (Book.books) {
+      Book.books.forEach((book, index) => {
+        const bookElement = `<p>${book.title}</p>
+    <p>${book.author}</p>
+    <button class="remove" id="${index}">Remove</button>
+    <hr>`;
+        this.#bookDiv.innerHTML += bookElement;
+      });
+    }
+    removeBtns = document.querySelectorAll(".remove");
     removeBtns.forEach((remove) => {
-      remove.addEventListener('click', (e) => {
+      remove.addEventListener("click", (e) => {
         const bookIndex = e.target.id;
-        books.splice(bookIndex, 1);
-        displayBooks();
-        localStorage.setItem('books', JSON.stringify(books));
+        Book.remove(bookIndex);
+        localStorage.setItem("books", JSON.stringify(Book.books));
+        Book.displayBooks();
       });
     });
   }
 }
 
-const addBookBtn = document.querySelector('#add');
-addBookBtn.addEventListener('click', (e) => {
+const addBookBtn = document.querySelector("#add");
+addBookBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  const title = document.querySelector('#title').value;
-  const author = document.querySelector('#author').value;
-  books.push({ title, author });
-  displayBooks();
-  localStorage.setItem('books', JSON.stringify(books));
+  const title = document.querySelector("#title").value;
+  const author = document.querySelector("#author").value;
+  let book = new Book(title, author);
+  book.add();
+  Book.displayBooks();
+  localStorage.setItem("books", JSON.stringify(Book.books));
 });
 
-if (localStorage.getItem('books') !== null) {
-  books = JSON.parse(localStorage.getItem('books'));
+if (localStorage.getItem("books") !== null) {
+  Book.books = JSON.parse(localStorage.getItem("books"));
 }
 
-displayBooks();
+Book.displayBooks();
