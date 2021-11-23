36be5147b8f1b298c5d923e25fe5771ee39a2f97
George Magdy <georgtriple@gmail.com>
2021-11-23 18:43:30 +0200

    Fix linter errors

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 window.onload = () => {
-  let removeBtns = document.querySelectorAll(".remove");
+  let removeBtns = document.querySelectorAll('.remove');
   class Book {
     static books = [];
 
-    static #bookDiv = document.querySelector("#books");
+    static #bookDiv = document.querySelector('#books');
 
     constructor(title, author) {
       this.title = title;
@@ -19,9 +19,9 @@ window.onload = () => {
     }
 
     static displayBooks() {
-      this.#bookDiv.innerHTML = "";
+      this.#bookDiv.innerHTML = '';
       if (Book.books.length === 0) {
-        this.#bookDiv.innerHTML = "No book added yet.";
+        this.#bookDiv.innerHTML = 'No book added yet.';
       }
       if (Book.books) {
         Book.books.forEach((book, index) => {
@@ -30,31 +30,31 @@ window.onload = () => {
           this.#bookDiv.innerHTML += bookElement;
         });
       }
-      removeBtns = document.querySelectorAll(".remove");
+      removeBtns = document.querySelectorAll('.remove');
       removeBtns.forEach((remove) => {
-        remove.addEventListener("click", (e) => {
+        remove.addEventListener('click', (e) => {
           const bookIndex = e.target.id;
           Book.remove(bookIndex);
-          localStorage.setItem("books", JSON.stringify(Book.books));
+          localStorage.setItem('books', JSON.stringify(Book.books));
           Book.displayBooks();
         });
       });
     }
   }
 
-  const addBookBtn = document.querySelector("#add");
-  addBookBtn.addEventListener("click", (e) => {
+  const addBookBtn = document.querySelector('#add');
+  addBookBtn.addEventListener('click', (e) => {
     e.preventDefault();
-    const title = document.querySelector("#title").value;
-    const author = document.querySelector("#author").value;
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
     const book = new Book(title, author);
     book.add();
     Book.displayBooks();
-    localStorage.setItem("books", JSON.stringify(Book.books));
+    localStorage.setItem('books', JSON.stringify(Book.books));
   });
 
-  if (localStorage.getItem("books") !== null) {
-    Book.books = JSON.parse(localStorage.getItem("books"));
+  if (localStorage.getItem('books') !== null) {
+    Book.books = JSON.parse(localStorage.getItem('books'));
   }
 
   Book.displayBooks();
